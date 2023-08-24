b3812f3ba2c1c2893acf1e0ebf85d3127af9998a
Gastòn Fernandez <gastonmfdz@gmal.com>
2023-08-24 17:20:04 -0300

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sat-fe';
+  title = 'herramientas';
+  url = ''
+
+  submit(){
+    
+  }
 }
